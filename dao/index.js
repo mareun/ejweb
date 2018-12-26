2d5b97214e99c12798d3beabac6b434d7355df3a
mareun <44865268+mareun@users.noreply.github.com>
2018-12-26 16:24:25 +0900

    1226

---
+++ dao/index.js
@@ -0,0 +1,24 @@
+var express = require
+var path = require('path')
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+						host: 'localhost',
+						port: 3306,
+                        user: 'root',
+                        password: 'dmswl0426!',
+                        database: 'ejdb'
+                        });
+
+connection.connect();
+
+connection.query('SELECT * from Persons', function(err, rows, fields){
+    if (!err)
+    console.log('The solution is: ', rows);
+    else
+    console.log('Error while performing Query.', err);
+
+});
+
+connection.end();
